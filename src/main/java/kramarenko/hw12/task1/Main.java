75164018c2d8f52d09ea1cd4438e14f7c4a5eb94
AleksandrKrm <kramarenko.alex27@gmail.com>
2021-09-11 13:30:45 +0300

    HW 12

--- src/main/java/kramarenko/hw12/task1/Main.java
+++ src/main/java/kramarenko/hw12/task1/Main.java
@@ -1,4 +1,14 @@
-package kramarenko.hw12;
+package kramarenko.hw12.task1;
 
 public class Main {
+    public static void main (String[] args){
+        Exception exception = new Exception("String");
+        try {
+            throw exception;
+        } catch (Exception e) {
+            System.out.println(exception);
+        } finally {
+            System.out.println("Finally");
+        }
+    }
 }
--- src/main/java/kramarenko/hw12/task2/TwoMethods.java
+++ src/main/java/kramarenko/hw12/task2/TwoMethods.java
@@ -1,7 +1,24 @@
 package kramarenko.hw12.task2;
 import java.util.Scanner;
-public class twoMethods {
+public class TwoMethods {
     public static void main(String[] args){
-        
+        TwoMethods object = new TwoMethods();
+    try(Scanner scan = new Scanner(System.in)){
+        object.f(scan.nextLine());
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    }
+    public void f (String smth) throws Exception {
+        try{
+            System.out.println( f (smth));
+        } catch (StringIndexOutOfBoundsException e){
+            throw new Exception();
+        }
+    }
+    public char g (String text) {
+        if (text.length()<1)
+            throw new IndexOutOfBoundsException();
+        return toString().charAt(0);
     }
 }
