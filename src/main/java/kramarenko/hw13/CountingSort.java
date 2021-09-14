883a25f910143f1227cdf7f6134d9e3e881f7124
AleksandrKrm <kramarenko.alex27@gmail.com>
2021-09-14 21:50:57 +0300

    HW 13 sort

--- src/main/java/kramarenko/hw13/CountingSort.java
+++ src/main/java/kramarenko/hw13/CountingSort.java
@@ -1,3 +1,60 @@
 package kramarenko.hw13;
+/*Сортировка подсчетом подразумевает создание корзин , в каждой из которой хранится
+количество элементов исходного массива, значение которых совпадает с индексом корзины. Соответственно,
+ нужно иметь корзины, индексы которых будут от минимального значения массива до максимального.
 
+При использовании массива для хранения корзин индексы будут от 0 до X. Чтобы при этом можно было
+работать с отрицательными числами, а также чтобы не хранить ненужные значения от 0 до минимального
+значения из массива, имеет смысл перед сортировкой (или непосредственно перед добавлением в корзину)
+ вычесть из всех элементов массива минимум, а после сортировки (при извлечении из корзины) - добавить
+ его обратно.
+*/
 public class CountingSort {
+    void sort(char arr[])
+    {
+        int n = arr.length;
+
+        // The output character array that will have sorted arr
+        char output[] = new char[n];
+
+        // Create a count array to store count of individual
+        // characters and initialize count array as 0
+        int count[] = new int[256];
+        for (int i = 0; i < 256; ++i)
+            count[i] = 0;
+
+        // store count of each character
+        for (int i = 0; i < n; ++i)
+            ++count[arr[i]];
+        // Change count[i] so that count[i] now contains actual
+        // position of this character in output array
+        for (int i = 1; i <= 255; ++i)
+            count[i] += count[i - 1];
+
+        // Build the output character array
+        // To make it stable we are operating in reverse order.
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[arr[i]] - 1] = arr[i];
+            --count[arr[i]];
+        }
+
+        // Copy the output array to arr, so that arr now
+        // contains sorted characters
+        for (int i = 0; i < n; ++i)
+            arr[i] = output[i];
+    }
+
+    // Driver method
+    public static void main(String args[])
+    {
+        CountingSort ob = new CountingSort();
+        char arr[] = { 'g', 'e', 'e', 'k', 's', 'f', 'o',
+                'r', 'g', 'e', 'e', 'k', 's' };
+
+        ob.sort(arr);
+        System.out.print("Sorted character array is ");
+        for (int i = 0; i < arr.length; ++i)
+            System.out.print(arr[i]);
+    }
+}
+
